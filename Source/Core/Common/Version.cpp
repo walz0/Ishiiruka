70328610bd751858d5677576dd3b2ebf9ced37a6
Nikhil Narayana <nikhil.narayana@live.com>
2024-07-08 20:56:06 -0700

    release: v3.4.1 | v3.4.3

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -23,6 +23,9 @@
 //#else
 //" " BUILD_TYPE_STR " " SCM_DESC_STR;
 //#endif
+// release commit notes
+// if releasing both builds, use `release: {combined_version}` or `release: {netplay_version} | {playback_version}`
+// for releasing a single build, use `release(netplay): {netplay_version}` or `release(playback): {playback_version}`
 #ifndef IS_PLAYBACK
 #define SLIPPI_REV_STR "3.4.1" // netplay version
 #else
